9dc041ad7993a64aaa5c854cc32b9b4f104f90cc
Dinesh Kumar Giri <66900440+Dinesh-kumar-Giri@users.noreply.github.com>
2022-02-04 02:29:03 +0530

    add the api and using the satate

--- src/Components/News.js
+++ src/Components/News.js
@@ -1,20 +1,32 @@
 // import PropTypes from 'prop-types';
-import React, { Component } from 'react';
-import NewsItem from './NewsItem';
+import React, { Component } from "react";
+import NewsItem from "./NewsItem";
 
 export class News extends Component {
-
-//   static propTypes = {};
+    // static propTypes = {};
+    constructor(){
+        super();
+        console.log("hello iam construcor from news component")
+    }
 
   render() {
-    return <div>
-        this is news Component app
-        <NewsItem/>
-        <NewsItem/>
-        <NewsItem/>
-        <NewsItem/>
+    return (
+      <div className="container my-1">
+        <h2>News-Top headline</h2>
+        <div className="row">
+          <div className="col-md-4">
+            <NewsItem title="my title giri" dis="hii" urlImage="https://ichef.bbci.co.uk/live-experience/cps/624/cpsprodpb/13590/production/_97584297_breaking_news.png" />
+          </div>
+          <div className="col-md-4">
+            <NewsItem title="my title" dis="description" />
+          </div>
+          <div className="col-md-4">
+            <NewsItem title="my title" dis="description" />
+          </div>
+        </div>
 
-    </div>;
+      </div>
+    );
   }
 }
 
--- src/Components/NewsItem.js
+++ src/Components/NewsItem.js
@@ -1,10 +1,25 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 export class NewsItem extends Component {
+    
   render() {
-    return <div>
-        this is NewsItem hey giri
-    </div>;
+      let {title,dis,urlImage}=this.props;
+    return (
+      <div className="my-2">
+        <div className="card" style={{width: "18rem"}}>
+          <img src={urlImage} className="card-img-top" alt="..." />
+          <div className="card-body">
+            <h5 className="card-title">{title}</h5>
+            <p className={dis}>
+              
+            </p>
+            <a href="/" className="btn-sm btn-primary">
+              Read more
+            </a>
+          </div>
+        </div>
+      </div>
+    );
   }
 }
 
