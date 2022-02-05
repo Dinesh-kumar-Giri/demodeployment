4d90038eabe28a5d34809d5f40f0e9c08cabb4c0
Dinesh Kumar Giri <66900440+Dinesh-kumar-Giri@users.noreply.github.com>
2022-02-05 14:48:06 +0530

    add logic news.js

--- src/Components/News.js
+++ src/Components/News.js
@@ -11,20 +11,50 @@ export class News extends Component {
         console.log("hello iam construcor from news component");
         this.state={
             articles:[],
-            loading:true
+            loading:true,
+            page:1
         }
     }
     async componentDidMount()
     {
       console.log("cdm");
-      let url="https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=630e5818d23a47829b60e8993959ffe9"
+      let url="https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=630e5818d23a47829b60e8993959ffe9&page=1pageSize=20";
       let data =await fetch(url);
       let parsedData=await data.json()
       console.log(parsedData)
-      this.setState({articles:parsedData.articles})
+      this.setState({articles:parsedData.articles,totalResults:parsedData.totalResults})
     }
+   handelPreClick=async ()=>{
+      console.log("younc")
+      let url=`https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=630e5818d23a47829b60e8993959ffe9&page=${this.state.page-1}&pageSize=20 `
+      let data =await fetch(url);
+      let parsedData=await data.json()
+      console.log(parsedData)
+      this.setState({
+        page: this.state.page-1,
+      })
+     
+    }
+    handelNexClick= async ()=>{
+      console.log("next");
+      if(this.state.page+1>Math.ceil(this.state.totalResults/20))
+      {
+
+      }
+      else{
+
+        let url=`https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=630e5818d23a47829b60e8993959ffe9&page=${this.state.page+1}&pageSize=20`
+        let data =await fetch(url);
+        let parsedData=await data.json()
+        console.log(parsedData)
+        this.setState({
+          page: this.state.page+1,
+        })
+      }
+    }
+
   render() {
-    console.log("i am render merhid")
+    // console.log("i am render merhid")
 
     return (
       <div className="container my-1">
@@ -38,6 +68,11 @@ export class News extends Component {
             <NewsItem  title={element.title?element.title.slice(0,30):" "} des={element.description?element.description.slice(0,80):" "} urlImage={element.urlToImage} newsUrl={element.url} />
           </div>
         })}
+        
+        <div className="container d-flex justify-content-between  ">
+        <button disabled={this.state.page<=1} type="button" class="btn btn-dark" onClick={this.handelPreClick} >previous</button>
+        <button type="button" class="btn btn-dark" onClick={this.handelNexClick} >Next</button>
+        </div>
          
           
         </div>
